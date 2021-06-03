ec1f6fcb281ebef1cfba9abf3b5e54996abd7e32
SeanPRyan <sean23397@gmail.com>
2021-06-03 12:25:32 +0100

    🎨🖌

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index(): 
     return render_template("index.html")
+def image():  
+    return app.send_static_file('index.html')
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/payment')
+def payment(): 
+    return render_template("payment.html")
